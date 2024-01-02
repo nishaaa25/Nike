eeda7c2681a3490f9aed1c2c3f80f3aed01b77b5
nishaaa25 <sahnisha2505@gmail.com>
2024-01-02 20:43:18 +0530

    added homepage

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default {
-  base: '/Nike/',
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/Nike/",
+  plugins: [react()],
+});
